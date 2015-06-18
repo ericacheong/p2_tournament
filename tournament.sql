63306143f889f5f6c1cdf56da61a650bfc050193
Erica Cheong <erica.clw@gmail.com>
2015-06-19 00:51:01 +0800

    Update match_count

--- tournament.sql
+++ tournament.sql
@@ -4,6 +4,7 @@
 -- Erica Cheong
 -- Date: 17 Jun 2015
 
+drop database tournament;
 
 -- create database
 create DATABASE tournament;
@@ -16,31 +17,44 @@ create TABLE players ( id serial primary key,
 					   );
 
 -- match table
-create TABLE matches ( p1 integer references players,
+create TABLE matches ( id serial primary key,
+					   p1 integer references players,
 					   p2 integer references players,
 					   winner integer references players
 					   );
 
--- view 1: number of matches each player has played
-create view match_count as 
-	select p1, count(*) 
-	from matches
-	group by p1;
+-- view 1: number of matches each player has playedß
+create view match_count as
+	select players.id, count(*)
+	from matches, players
+	where players.id in (p1, p2) 
+	group by players.id;
 
 -- view 2: number of wins for each player
 create view win_count as
 	select winner, count(*)
-	from matches
-	group by p1;
-
-
+	from matches left join players
+	on matches.winner = players.id
+	group by winner;
 
 -- view 3: player standings
 create view standings as
 	select players.id, players.name, win_count.count as wins, match_count.count as match
 	from players 
 	left join win_count on players.id = win_count.winner
-	left join match_count on players.id = match_count.p1
+	left join match_count on players.id = match_count.id;
+
+insert into players (name) values ('Danny Edwards');
+insert into players (name) values ('TOM PURTZER');
+insert into players (name) values ('KEN GREEN');
+insert into players (name) values ('STEVE PATE');
+
+select * from players;
+
+insert into matches values (1,1,2,1);
+insert into matches values (2,3,4,4);
+insert into matches values (3,1,4,4);
+insert into matches values (4,2,3,2);
 
 
 
